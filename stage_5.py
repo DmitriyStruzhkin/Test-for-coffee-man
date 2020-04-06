2d24372f8d08d9bb208a762318348c0287577832
Dmitriy Struzhkin <struzhkin.dmitriy@gmail.comgit config --global user.email struzhkin.dmitriy@gmail.comgit config --global user.email struzhkin.dmitriy@gmail.com>
2020-04-06 11:16:45 +0300

    Project without class method

--- stage_5.py
+++ stage_5.py
@@ -1,27 +1,31 @@
-# Write your code here
-# print("Write how many cups of coffee you will need:")
-# cups = int(input())
-# print(f'''For {cups} cups of coffee you will need:
-# {cups * 200} ml of water
-# {cups * 50} ml of milk
-# {cups * 15} g of coffee beans''')
 def have_printer():
     print(f'''The coffee machine has:
 {machine_list[0]} of water
 {machine_list[1]} of milk
 {machine_list[2]} of coffee beans
 {machine_list[3]} of disposable cups
-{machine_list[4]} of money''', '\n')
+${machine_list[4]} of money''', '\n')
+def action_buy():
+    buy_list = {1: [250, 0, 16, 1, -4], 2: [350, 75, 20, 1, -7], 3: [200, 100, 12, 1, -6]}
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    buy_number = input()
+    if buy_number == 'back':
+        return
+    resource_status = 1
+    for i in range(len(machine_list)):
+        if machine_list[i] - buy_list[int(buy_number)][i] < 0:
+            print(f"Sorry, not enough {resource_list[i]}!")
+            resource_status = 0
+            print()
+    if resource_status == 1:
+        for i in range(len(machine_list)):
+           machine_list[i] = machine_list[i] - buy_list[int(buy_number)][i]
+        print("I have enough resources, making you a coffee!")
+        print()
 
 def action_move(action):
-    buy_list = {1: [250, 0, 16, 1, -4], 2: [350, 75, 20, 1, -7], 3: [200, 100, 12, 1, -6]}
     if action == 'buy':
-        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-        buy_number = int(input())
-        for i in range(len(machine_list)):
-            machine_list[i] = machine_list[i] - buy_list[buy_number][i]
-            print(machine_list[i], buy_list[buy_number][i])
-        have_printer()
+        action_buy()
     elif action == 'fill':
         print("Write how many ml of water do you want to add:")
         machine_list[0] += int(input())
@@ -31,24 +35,39 @@ def action_move(action):
         machine_list[2] += int(input())
         print("Write how many disposable cups of coffee do you want to add:")
         machine_list[3] += int(input())
-        have_printer()
+        print()
     elif action == 'take':
         print(f"I gave you ${machine_list[4]}", '\n')
         machine_list[4] = 0
-        have_printer()
+    elif action == 'remaining':
+        if machine_list[4] > 0:
+            have_printer()
+        else:
+            print(f'''The coffee machine has:
+{machine_list[0]} of water
+{machine_list[1]} of milk
+{machine_list[2]} of coffee beans
+{machine_list[3]} of disposable cups
+{machine_list[4]} of money''', '\n')
 
-water = 1200
+
+water = 400
 milk = 540
-beans = 120
-dis_caps = 9
+coffee_beans = 120
+disposable_cups = 9
 money = 550
-machine_list = [water, milk, beans, dis_caps, money]
+resource_list = ["water", "milk", "coffee_beans", "disposable_cups", 'money']
+machine_list = [water, milk, coffee_beans, disposable_cups, money]
 
-have_printer()
-
-print("Write action (buy, fill, take):")
+print("Write action (buy, fill, take, remaining, exit):")
 action = input()
+print()
 action_move(action)
+while action != 'exit':
+    print("Write action (buy, fill, take, remaining, exit):")
+    action = input()
+    print()
+    action_move(action)
 
 # espresso = [250, 0, 16, 1, -4]
 # latte = [350, 75, 20, 1, -7]
